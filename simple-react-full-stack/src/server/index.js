7358fb5ce4052e4ea2769e1adf15a329a720bb16
krohak <rohaksinghal14@gmail.com>
2018-10-18 11:55:32 +0800

    Working react full stack

---
+++ simple-react-full-stack/src/server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const os = require('os');
+
+const db = require('./db')
+
+const app = express();
+
+app.use(express.static('dist'));
+app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.listen(8080, () => console.log('Listening on port 8080!'));
